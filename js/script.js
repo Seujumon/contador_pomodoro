a9f75db520f9376ee6c11ea51093998070726154
Seujumon <jseura@gmail.com>
2023-08-07 00:52:29 -0300

    Funcionalidad terminada
    Agregué más funciones de las deseadas. Hay un audio libre de derechos, Botones para iniciar el cronometro, detenerlo/pausarlo, continuar desde donde quedó, y detener el audio. Agregué un campo para los segundos y los label que no pensaba ingresar. También hay una función para que el tiempo del cronómetro se actualice a medida que se carga

--- js/script.js
+++ js/script.js
@@ -1,12 +1,43 @@
 let mostrarTotal = document.getElementById('total');
 let mostrarCronometro = document.getElementById('cronometro');
-let tiempoIngresado = 1;
-let sound = new Audio('./sound/cancion.wav');
+let tiempoIngresadomins = document.getElementById('mins');
+let tiempoIngresadosegs = document.getElementById('segs');
+let clicIniciar = document.getElementById('inicio');
+let clicContinuar = document.getElementById('continuar');
+let clicAudio = document.getElementById('audio');
+let clicDetener = document.getElementById('detener');
+let sound = new Audio('./sound/trumpets-for-life.mp3');
 
-let tiempo = tiempoIngresado*60;
+let intervalo;
 
+let tiempo;
+
+function mostrarTiempoSeteado(){
+	tiempo = (parseInt(tiempoIngresadomins.value, 10)*60)+ parseInt(tiempoIngresadosegs.value);
+	let minutos= Math.trunc(tiempo/60);
+	let segundos = tiempo%60;
+	let addCeromin = "";
+	if(minutos<10){
+		addCeromin = "0"
+	} else{
+		addCeromin = ""
+	}
+	let addCeroseg = "";
+	if(segundos<10){
+		addCeroseg = "0"
+	} else{
+		addCeroseg = ""
+	}
+	mostrarCronometro.innerHTML = addCeromin + minutos + " : " + addCeroseg+  segundos;
+}
+
+function asignarTiempo(){
+	tiempo = (parseInt(tiempoIngresadomins.value, 10)*60)+ parseInt(tiempoIngresadosegs.value);
+
+}
 
 function imprimirCronometro(){
+
 	let minutos= Math.trunc(tiempo/60);
 	let segundos = tiempo%60;
 	let addCeromin = "";
@@ -30,8 +61,29 @@ function imprimirCronometro(){
 		sound.play();
 	}
 }
-/**
-Puedo poner esto en una funcion, y referenciarla en cuanto el usuario 
-haga clic en el botón**/
-let intervalo = setInterval(imprimirCronometro, 1000);
+
+function iniciar(){
+	clearInterval(seteo);
+	clearInterval(intervalo);
+	asignarTiempo();
+	intervalo = setInterval(imprimirCronometro, 1000);
+}
+function stop(){
+	clearInterval(intervalo);
+}
+
+function continuar(){
+	clearInterval(intervalo);
+	intervalo = setInterval(imprimirCronometro, 1000);
+}
+function detenerAudio(){
+	sound.pause();
+}
+let seteo = setInterval(mostrarTiempoSeteado,1000);
+
+clicIniciar.onclick = iniciar;
+clicContinuar.onclick = continuar;
+clicDetener.onclick = stop;
+clicAudio.onclick = detenerAudio;
+
 
