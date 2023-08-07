6bbf63d64691b13eee397301000559ed5f26fc55
Seujumon <jseura@gmail.com>
2023-08-06 22:13:49 -0300

    Cronometro funcionando
    Ya funciona el cronometro, por el momento con un tiempo hardcodeado. TODO agregar un input para ingresar el tiempo, un botón que inicie el cronómetro, y un sonido cuando llegue a 0

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
-function saludo(){
-	console.log("hola")
-}
+let mostrarTotal = document.getElementById('total');
+let mostrarCronometro = document.getElementById('cronometro');
 
-setTimeout(saludo,1000);
+
+let tiempo = 1*60;
+
+
+function imprimirCronometro(){
+	let minutos= Math.trunc(tiempo/60);
+	let segundos = tiempo%60;
+	let addCeromin = "";
+	if(minutos<10){
+		addCeromin = "0"
+	} else{
+		addCeromin = ""
+	}
+	let addCeroseg = "";
+	if(segundos<10){
+		addCeroseg = "0"
+	} else{
+		addCeroseg = ""
+	}
+	mostrarCronometro.innerHTML = addCeromin + minutos + " : " + addCeroseg+  segundos;
+	tiempo--;	
+
+	
+	if(tiempo<0){
+		clearInterval(intervalo)
+	}
+}
+let intervalo = setInterval(imprimirCronometro, 1000);
+
